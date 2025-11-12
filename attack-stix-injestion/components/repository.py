671292f319b5c50800e836256403651e0b9f64c1
Nathan Jin <nathan.jin@illumio.com>
2025-11-11 19:58:15 -0800

    more refactoring repository, moving relationships out

--- attack-stix-injestion/components/repository.py
+++ attack-stix-injestion/components/repository.py
@@ -14,53 +14,85 @@ class Repository:
         config.DATABASE_URL = 'bolt://:@localhost:7687'  # default
         install_all_labels()
         self.cached_instances = {}
-        
-        # enusre resource_manager exists for all classes
-        with db.transaction: 
-            models = set(map(lambda model_class: model_class.__name__, MODEL_LIST))
-            for r in ResourceManager.nodes.all():
-                models.remove(r.resource)
+        self._instantiate_missing_resource_managers()
             
-            for instantiate_models in models:
-                r = ResourceManager(resource=instantiate_models)
-                r.save()
-                Repository._resource_manager_cache[instantiate_models] = r
     def load_database(self, type_id_resource_mapping, domain):
         nodes_count, _ = db.cypher_query("MATCH (n) RETURN count(n)")
         rels_count, _ = db.cypher_query("MATCH ()-[r]->() RETURN count(r)")
 
         print(f"Nodes: {nodes_count[0][0]}, Relationships: {rels_count[0][0]}")
         filtered_objects = self.filter_resources(type_id_resource_mapping, domain)
-        # custom id defined by parser
+        
+        # caches to help resolve relationships references, neccessary / optimization
+        relationship_queue = filtered_objects['relationship'].pop("added")
+        
+        # temporary extraction of relationships
+        for op, matrix_batch in filtered_objects["x-mitre-matrix"].items():
+            if op != "removed":
+                for matrix in matrix_batch:
+                    for ref in matrix["tactic_refs"]:
+                        relationship_queue.append({
+                            "source_ref": matrix["stix_uuid"],
+                            "target_ref": ref,
+                            "relationship_type": "contains"
+                        })
         with db.transaction: 
-            # caches to help resolve relationships references, neccessary / optimization
-            relationship_queue = filtered_objects['relationship'].pop("added")
-            tactic_cache = {}
-            technique_tactic_relationships = []
-            
             for str_type, batch in filtered_objects.items():
-                self.perform_batch_operation(str_type, batch, domain, tactic_cache, relationship_queue, technique_tactic_relationships)
-            
-            # resolve tactic names using tactic cache for tactic relationships
-            for tactic_to_technique in technique_tactic_relationships:
-                resolved_source_ref = None
-                tactic_name = tactic_to_technique.pop("target_tactic_name")
-                try:
-                    resolved_source_ref = tactic_cache[tactic_name]
-                except KeyError:
-                    tactic = Tactic.nodes.get(name=tactic_name)
-                    resolved_source_ref = tactic.stix_uuid
-                    # add to cache for relationship
-                    self.cached_instances[resolved_source_ref] = tactic
-                tactic_to_technique["target_ref"] = resolved_source_ref
-                relationship_queue.append(tactic_to_technique)
+                model_type = find_model_from_type(str_type)
+                rm_for_batch = self._get_resource_manager(str_type)
+                updated_mappin_for_batch = rm_for_batch.x_mitre_contents_serialized
+                removal_queue = batch.pop("removed")
                 
+                self.perform_batch_operation(updated_mappin_for_batch, model_type, batch)
+                for uuid in removal_queue:
+                    object_domains = updated_mappin_for_batch[uuid]["domains"]
+                    object_domains.remove(domain)
+                    if len(object_domains) == 0:
+                        model_type.nodes.get(stix_uuid=uuid).delete()
+                        updated_mappin_for_batch.pop(uuid)
+                    else:
+                       self.remove_label_from_node(uuid, clean_str(domain))
+                rm_for_batch.x_mitre_contents_serialized = updated_mappin_for_batch
+                rm_for_batch.save()
+            
+            
             # connect nodes at the end
+            self.add_tactic_technique_relationships(filtered_objects, relationship_queue)
             self.process_relationship_queue(relationship_queue)
         nodes_count, _ = db.cypher_query("MATCH (n) RETURN count(n)")
         rels_count, _ = db.cypher_query("MATCH ()-[r]->() RETURN count(r)")
         print(f"Nodes: {nodes_count[0][0]}, Relationships: {rels_count[0][0]}")
         # input("results: ")
+    
+    def add_tactic_technique_relationships(self, filtered_objects, relationship_queue):
+        # resolve tactic names using tactic cache for tactic relationships
+        tactic_name_cache = {}
+        for op, tactic_batch in filtered_objects["x-mitre-tactic"].items():
+            for tactic in tactic_batch:
+                tactic_name_cache[tactic["name"]] = tactic["stix_uuid"]
+        for op, technique_batch in filtered_objects["attack-pattern"].items():
+            for technique in technique_batch:
+                if "related_tactics" not in technique:
+                    continue
+                for tactic_name in technique["related_tactics"]:
+                    # specific patch-fix
+                    tactic_name = tactic_name.replace("-", " ")
+                    
+                    # resolve name
+                    resolved_source_ref = None
+                    try:
+                        resolved_source_ref = tactic_name_cache[tactic_name]
+                    except KeyError:
+                        tactic_obj = Tactic.nodes.get(name=tactic_name)
+                        resolved_source_ref = tactic_obj.stix_uuid
+                        self.cached_instances[resolved_source_ref] = tactic_obj
+            
+                    # add relationship queue
+                    relationship_queue.append({
+                        "source_ref": technique["stix_uuid"],
+                        "target_ref": resolved_source_ref,
+                        "relationship_type": "technique_of"
+                    })
     
     def process_relationship_queue(self, relationship_queue):
         # process relationship queue
@@ -70,8 +102,8 @@ class Repository:
             
             #temprory ics checks
             try:
-                Repository.type_from_stix_uuid(source_ref)
-                Repository.type_from_stix_uuid(target_ref)
+                self._type_from_stix_uuid(source_ref)
+                self._type_from_stix_uuid(target_ref)
             except Exception:
                 continue
             
@@ -107,22 +139,8 @@ class Repository:
         """
         results, meta = db.cypher_query(query, {'uuid': stix_uuid})
         
-    def perform_batch_operation(self, str_type, operation_batch, domain, tactic_cache, relationship_queue, technique_tactic_relationships):
-        model_type = find_model_from_type(str_type)
-        rm_for_batch = self.get_resource_manager(str_type)
-        mappin_for_batch = rm_for_batch.x_mitre_contents_serialized
+    def perform_batch_operation(self, mappin_for_batch, model_type, operation_batch):
         for operation, operation_data in operation_batch.items():
-            if operation == "removed":
-                for uuid in operation_data:
-                    object_domains = mappin_for_batch[uuid]["domains"]
-                    object_domains.remove(domain)
-                    if len(object_domains) == 0:
-                        model_type.nodes.get(stix_uuid=uuid).delete()
-                        mappin_for_batch.pop(uuid)
-                    else:
-                       self.remove_label_from_node(uuid, clean_str(domain))
-                continue
-            
             # add or update
             for obj_dict in operation_data:
                 
@@ -142,24 +160,6 @@ class Repository:
                 obj_instance, obj_class = self._instantiate_json(operation, model_type, obj_dict["stix_uuid"])
                 self._fill_model_with_dict(obj_instance, obj_dict)
                 obj_instance.save()
-                if obj_class is Tactic:
-                    tactic_cache[obj_instance.name] = obj_instance.stix_uuid
-                if obj_class is Matrix:
-                    for ref in obj_dict["tactic_refs"]:
-                        relationship_queue.append({
-                            "source_ref": obj_instance.stix_uuid,
-                            "target_ref": ref,
-                            "relationship_type": "contains"
-                        })
-                if obj_class is Technique and "related_tactics" in obj_dict:
-                    for tactic in obj_dict["related_tactics"]:
-                        # specific patch-fix
-                        tactic = tactic.replace("-", " ")
-                        technique_tactic_relationships.append({
-                            "source_ref": obj_instance.stix_uuid,
-                            "target_tactic_name": tactic,
-                            "relationship_type": "technique_of"
-                        })
                     
                 # add any (new) custom labels to object
                 # TODO: Clear labels if operation is updated
@@ -170,23 +170,13 @@ class Repository:
                     db.cypher_query(f"MATCH (n:{obj_class.__name__}) WHERE id(n)={obj_instance.element_id.split(":")[-1]} SET n:{label}")
 
                 self.cached_instances[obj_instance.stix_uuid] = obj_instance
-        rm_for_batch.x_mitre_contents_serialized = mappin_for_batch
-        rm_for_batch.save()
-                
-    def get_resource_manager(self, resource_type):
-        model_name = find_model_from_type(resource_type).__name__
-        if model_name in Repository._resource_manager_cache:
-            return Repository._resource_manager_cache[model_name]
-        extracted_rm =  ResourceManager.nodes.filter(resource=model_name).first()
-        Repository._resource_manager_cache[model_name] = extracted_rm
-        return extracted_rm
         
     # returns bundles of objects that need change
     def filter_resources(self, resource_mapping, domain):
         formatted_resources = {}
         outputdict = {}
         for type in resource_mapping:
-            resource_manager_map = self.get_resource_manager(type).x_mitre_contents_serialized
+            resource_manager_map = self._get_resource_manager(type).x_mitre_contents_serialized
             mappings = resource_mapping[type]
             filtered_resources = {
                 "updated": [],
@@ -213,7 +203,7 @@ class Repository:
             #debugging
             outputdict[type] = filtered_resources["removed"]
         print(outputdict)
-        input("removal for " + domain + ": ")
+        # input("removal for " + domain + ": ")
         return formatted_resources
     
     def _fill_model_with_dict(self, instantiatedModel, attribute_dict):
@@ -223,15 +213,32 @@ class Repository:
                 attribute_dict[att_name] = attribute_dict[att_name].lower()
             setattr(instantiatedModel, att_name, attribute_dict[att_name])
     
-    @classmethod
-    def type_from_stix_uuid(clz, uuid):
-        class_type, extracted_uuid = uuid.split("--")
-        return find_model_from_type(class_type), extracted_uuid
-    
     def _load_model_from_stix_uuid(self, uuid):
         try:
             return self.cached_instances[uuid]
         except KeyError:
-            model, _ = Repository.type_from_stix_uuid(uuid)
+            model, _ = self._type_from_stix_uuid(uuid)
             self.cached_instances[uuid] = model.nodes.get(stix_uuid=uuid)
             return self.cached_instances[uuid]
+    def _type_from_stix_uuid(self, uuid):
+        class_type, extracted_uuid = uuid.split("--")
+        return find_model_from_type(class_type), extracted_uuid
+    # ResourceManager
+    def _instantiate_missing_resource_managers(self):
+        with db.transaction: 
+            models = set(map(lambda model_class: model_class.__name__, MODEL_LIST))
+            for r in ResourceManager.nodes.all():
+                models.remove(r.resource)
+            
+            for instantiate_models in models:
+                r = ResourceManager(resource=instantiate_models)
+                r.save()
+                Repository._resource_manager_cache[instantiate_models] = r
+                
+    def _get_resource_manager(self, resource_type):
+        model_name = find_model_from_type(resource_type).__name__
+        if model_name in Repository._resource_manager_cache:
+            return Repository._resource_manager_cache[model_name]
+        extracted_rm =  ResourceManager.nodes.filter(resource=model_name).first()
+        Repository._resource_manager_cache[model_name] = extracted_rm
+        return extracted_rm
